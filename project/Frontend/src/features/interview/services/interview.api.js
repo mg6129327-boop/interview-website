bd7a8835b0b6bea3acb2ac162cf3e73270944725
Mridul <tumhara_github_email@gmail.com>
2026-06-18 15:52:22 +0530

    Fixed interview API URL

--- project/Frontend/src/features/interview/services/interview.api.js
+++ project/Frontend/src/features/interview/services/interview.api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true,
+    baseURL: "https://interview-website-zqu3.onrender.com",
+    withCredentials: true
 })
 
 
